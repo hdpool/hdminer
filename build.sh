dc432ebc40b32867f422560346411b4f24e7321c
hdpool <57438680+hdpool@users.noreply.github.com>
2019-12-02 17:48:08 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,25 +9,21 @@ TARGET="build-all"
 rm -rf ${TARGET} && mkdir ${TARGET}
 
 # windows
-rm -f hdminer.exe libminer_windows.a;
+rm -f libminer_windows.a;
 (cd miner; PRECC=x86_64-w64-mingw32- sh -x ./b.sh);
 CGO_ENABLED=1 GOOS=windows GOARCH=amd64 CC=x86_64-w64-mingw32-gcc ${GOROOT}/bin/go build -ldflags "-s -w -H windowsgui" -o ${TARGET}/hdminer.exe
 
 # linux 
-rm -f hdminer libminer.a 
+rm -f libminer.a 
 (cd miner; sh -x ./b.sh)
 CGO_ENABLED=1 GOOS=linux GOARCH=amd64 ${GOROOT}/bin/go build -ldflags "-s -w" -o ${TARGET}/hdproxy.linux
 
 # darwin
-# rm -f hdminer libminer_darwin.a
+# rm -f libminer_darwin.a
 # (cd miner; sh -x ./b.sh)
 # CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 ${GOROOT}/bin/go build -gccgoflags "-s -w" -o ${TARGET}/hdproxy.mac
-# 
-# cp hdminer ${TARGET}/hdproxy.mac
-# mv hdminer ${TARGET}/center_proxy.mac
 
 # upx 
-(cd ${TARGET}; upx -9 -v *)
 (cd ${TARGET}; echo 'hdproxy.exe center_proxy.exe' | xargs -n 1 cp hdminer.exe)
 (cd ${TARGET}; echo 'center_proxy.linux' | xargs -n 1 cp hdproxy.linux)
 # (cd ${TARGET}; echo 'center_proxy.mac' | xargs -n 1 cp hdproxy.mac)
